86861fe00652e8d0fc4317794fb2e6032c919ec4
Jonah Hollis <jonahquinnhollis@gmail.com>
2022-12-09 14:43:01 -0500

    updating solutions file

--- Week-02-Conditions_and_Loops/openclass.js
+++ Week-02-Conditions_and_Loops/openclass.js
@@ -59,7 +59,7 @@ for (i = 0; (i < 100) || (points >= 290); i++){
   if number is even + 5 pts
   if number is odd + 3 pts
   if score == 125, then - 25 pts (just once)
-  game ends on 100th turn or you reach > 290 pts, whichever first */
+  game ends on 100th turn or you reach > 290 pts, whichever first  */
   
   
   
@@ -74,15 +74,22 @@ for (i = 0; (i < 100) || (points >= 290); i++){
       points = 25
       pointsReset = true
     }else{
-      if (points % 2 === 0){
+      if (i % 2 === 0){
         points += 5
-      }else if (points % 2 !== 0) {
+      }else if (i % 2 !== 0) {
         points += 3
       }   
     }
     console.log('Turns: ' + i + ' Score: ' + points)
   }
   
-  
+
+
+let num = 0
+for(i = 0; num < 100; i++){
+    console.log(num)
+    num += 3
+}
+
 
   

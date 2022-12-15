638a30643db7de29447041bc5714d007136c3040
Jonah Hollis <jonahquinnhollis@gmail.com>
2022-12-15 12:22:31 -0500

    complete coding assignment for week 3

--- Week-03-Arrays_and_Functions/template/js/main.js
+++ Week-03-Arrays_and_Functions/template/js/main.js
@@ -114,12 +114,64 @@ greaterThan100([10])
 
 // 10. Write a function that takes an array of numbers and returns the average of all the elements in the array.
 
+function returnAverage (arr){
+    let sum = 0
+    for(i = 0; i < arr.length ; i++){
+        sum += arr[i]
+    }
+    console.log(sum/i)
+}
+
+returnAverage([1,2,3,4,5])
 
 // 11. Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
 
+function isFirstArrGreater(arr1,arr2){
+    let sum1 = 0
+    for(i = 0; i < arr1.length ; i++){
+        sum1 += arr1[i]
+    }
+    console.log(sum1/i)
+
+    let sum2 = 0
+    for(i = 0; i < arr2.length ; i++){
+        sum2 += arr2[i]
+    }
+    console.log(sum2/i)
+
+    if(sum1 > sum2){
+        console.log(true)
+    }else{
+        console.log(false)
+    }
+}
+
+isFirstArrGreater([1,2,3,4,5],[1,2,3,4,5,6])
 
 // 12. Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+
+let isHotOutside = true
+let moneyInPocket = 11.50
+
+function willBuyDrink(isHotOutside,moneyInPocket){
+    if(isHotOutside === true && (moneyInPocket > 10.50)){
+        console.log(true)
+    }else{
+        console.log(false)
+    }
+}
+
+willBuyDrink(isHotOutside,moneyInPocket)
 
 
 // 13. Create a function of your own that solves a problem. 
 // In comments, write what the function does and why you created it.
+
+function fahrenheitToCelsius(fahrenheit) {
+    // 32 is subtracted from the fahrenheit input, then divided by 1.8; this is the formula to convert fahrenheit to celsius. the result is then returned
+    return (fahrenheit - 32) / 1.8;
+}
+
+// initialize celsius as a variable, set it equal to the fahrenheitToCelsius function with a fahrenheit input of 100
+
+let celsius = fahrenheitToCelsius(100)

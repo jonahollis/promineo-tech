17d8d9e1b1f2cf4f433a287fff617bb3fff8eff2
Jonah Hollis <jonahquinnhollis@gmail.com>
2023-01-28 09:06:09 -0500

    adding initial mocha test for function cardValue

--- Week-06-DevTools_Debugging_and_Unit_Tests/war-card-game/js/index_test.js
+++ Week-06-DevTools_Debugging_and_Unit_Tests/war-card-game/js/index_test.js
@@ -1,10 +1,8 @@
 const expect = chai.expect
 
-describe('MyFunction', function(){
-    describe('#doSomething', function(){
-        it('should concatenate the two paramenters', function(){
-            const x = doSomething('Hello', 5)
-            expect(x).to.equal('Hello5')
-        })
+describe('#cardValue', function(){
+    it('randomly generate a number between 1 and 13 and return it', function(){
+        const x = cardValue()
+        expect(x).to.equal(x > 0 && x < 14)
     })
 })

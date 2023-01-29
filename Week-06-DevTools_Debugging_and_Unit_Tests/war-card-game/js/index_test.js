295aed2719dbd4070b3ade03cfba13f816318096
Jonah Hollis <jonahquinnhollis@gmail.com>
2023-01-29 09:09:03 -0500

    adding alt tags to images for accessibility

--- Week-06-DevTools_Debugging_and_Unit_Tests/war-card-game/js/index_test.js
+++ Week-06-DevTools_Debugging_and_Unit_Tests/war-card-game/js/index_test.js
@@ -6,3 +6,5 @@ describe('#cardValue', function(){
         expect(x).to.equal(x > 0 && x < 14)
     })
 })
+
+// https://stackoverflow.com/questions/26704677/how-do-i-test-if-a-function-calls-a-specific-method-function

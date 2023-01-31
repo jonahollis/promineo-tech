c9eb1d1eab3ef8a125e7319f33b4aed7a3706fbd
Jonah Hollis <jonahquinnhollis@gmail.com>
2023-01-31 09:29:36 -0500

    adding full node_modules dir to verify working unit tests in mocha and chai

--- Week-06-DevTools_Debugging_and_Unit_Tests/war-card-game/js/index_test.js
+++ Week-06-DevTools_Debugging_and_Unit_Tests/war-card-game/js/index_test.js
@@ -1,9 +1,11 @@
 const expect = chai.expect
 
 describe('#cardValue', function(){
-    it('should randomly generate a number between 1 and 13 and return it', function(){
+    it('should randomly generate a whole number between 1 and 13 and return it', function(){
         const x = cardValue()
         expect(Number.isInteger(x)).to.be.true
+        expect(x > 0).to.be.true
+        expect(x < 14).to.be.true
     })
 
 it('should not accept any parameters', function(){
